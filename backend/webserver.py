0e75eca1d44371b8e055331cff05d90561082279
Ubuntu <ubuntu@ip-172-31-60-94.ec2.internal>
2021-02-17 21:31:54 +0000

    Only loads waitress on explicit run

--- backend/webserver.py
+++ backend/webserver.py
@@ -8,6 +8,7 @@ from flask import Flask
 from flask import render_template, request, send_file, make_response, jsonify, send_from_directory
 from flask_cors import CORS, cross_origin
 from flask_socketio import SocketIO
+from waitress import serve
 from .classify_epilepsy import EpilepsyClassifier
 
 ## Configure Flask app
@@ -23,6 +24,12 @@ cors = CORS(app)
 # Access-Control-Allow-Methods: POST
 # Access-Control-Allow-Headers: Content-Type, Authorization
 
+@app.route("/", methods=["GET"])
+def sanity_check():
+    """Just a ping"""
+    response = make_response("pong", 200)
+    response.mimetype = "text/plain"
+    return response
 
 def classify_on_edf(filepath):
     """Runs the (long) method to classify epileptic
@@ -54,3 +61,7 @@ def upload_edf():
     # response = make_response(jsonify({"annoted_file_url": save_file}))
     return send_from_directory(directory="/tmp/", filename=save_file)
 
+if __name__ == "__main__":
+    # serve called when run as a module
+    # so it doesn't interfere with flask dev
+    serve(app, listen='*:8080')
