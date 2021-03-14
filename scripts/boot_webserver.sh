1dbd158be9cb4be650869793af3715ca061c31d9
Phillip K <phalcon@localhost.localdomain>
2021-03-14 18:37:00 -0400

    Reconfigured environment.yml to use the name ng and auto install pip packages as well

--- scripts/boot_webserver.sh
+++ scripts/boot_webserver.sh
@@ -2,6 +2,6 @@
 cd /home/ubuntu/Neurogram
 if [ -f "/home/ubuntu/anaconda3/etc/profile.d/conda.sh" ]; then
     . "/home/ubuntu/anaconda3/etc/profile.d/conda.sh"
-    CONDA_CHANGEPS1=false conda activate mne
+    CONDA_CHANGEPS1=false conda activate ng
 fi
 python -m backend.webserver
--- scripts/ng_dev_environment.sh
+++ scripts/ng_dev_environment.sh
@@ -21,18 +21,18 @@ tmux resize-pane -t 2 -y 40
 # backend
 tmux select-pane -t 0
 tmux send-keys 'cd $NG' C-m
-tmux send-keys 'conda activate mne' C-m
+tmux send-keys 'conda activate ng' C-m
 tmux send-keys 'export FLASK_APP=backend.webserver' C-m
 tmux send-keys 'export FLASK_ENV=development' C-m
 tmux send-keys 'flask run' C-m
 # frontend
 tmux select-pane -t 1
-tmux send-keys 'conda activate mne' C-m
+tmux send-keys 'conda activate ng' C-m
 tmux send-keys 'cd $NG/react' C-m 'npm start' C-m
 # general
 tmux select-pane -t 2
 tmux send-keys 'cd $NG' C-m
-tmux send-keys 'conda activate mne' C-m
+tmux send-keys 'conda activate ng' C-m
 tmux send-keys 'code ./backend' C-m
 tmux send-keys 'code ./react' C-m
 
