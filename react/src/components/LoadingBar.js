d22144832f40f3089378493fe024dda1f0ab4324
Phillip K <phalcon@localhost.localdomain>
2021-04-18 15:49:23 -0400

    Starting the loading bar

---
+++ react/src/components/LoadingBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+class LoadingBar extends React.Component {
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default LoadingBar;
